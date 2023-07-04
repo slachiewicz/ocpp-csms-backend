0c04e7db01dd68a21e5ccede5f00cc3f1097ef9a
heroyooki <heroyooki@gmail.com>
2023-07-04 20:38:56 +0300

    Introduced startup event

--- src/manager/main.py
+++ src/manager/main.py
@@ -7,9 +7,14 @@ from core.settings import EVENTS_QUEUE_NAME
 from manager.controllers.status import status_router
 from manager.services.events import process_event
 
-asyncio.ensure_future(
-    start_consume(queue_name=EVENTS_QUEUE_NAME, on_message=process_event)
-)
 app = FastAPI()
+
+
+@app.on_event("startup")
+async def startup():
+    asyncio.ensure_future(
+        start_consume(queue_name=EVENTS_QUEUE_NAME, on_message=process_event)
+    )
+
 
 app.include_router(status_router)
