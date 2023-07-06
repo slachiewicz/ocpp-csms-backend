284bce13f8d94fa0cb69acf553d531bcf12f85e0
heroyooki <heroyooki@gmail.com>
2023-07-06 16:22:42 +0300

    Added a few comments into publishers methods

--- src/sse/publisher.py
+++ src/sse/publisher.py
@@ -10,13 +10,23 @@ from sse import observer as obs
 class Publisher:
     observers: List[obs.Observer] = []
 
-    async def ensure_observers(self):
+    async def ensure_observers(self) -> None:
+        """
+        Remove inactive observers from the 'observers' list.
+        :return:
+        """
         for observer in self.observers:
             if await observer.request.is_disconnected():
                 self.observers.remove(observer)
                 del observer
 
     def publish(self, func) -> Callable:
+        """
+        Publish new event for all observers in the list
+        :param func:
+        :return:
+        """
+
         @wraps(func)
         async def wrapper(*args, **kwargs):
             event = await func(*args, **kwargs)
@@ -27,7 +37,6 @@ class Publisher:
         return wrapper
 
     async def add_observer(self, observer: obs.Observer) -> None:
-        # Care about disconnected observers
         await self.ensure_observers()
         self.observers.append(observer)
 
