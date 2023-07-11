057b4a9b3608d058607bc69e2e50dd86af2cc5a0
heroyooki <heroyooki@gmail.com>
2023-07-11 21:28:06 +0300

    Additional fields for charge points

--- src/manager/models/charge_point.py
+++ src/manager/models/charge_point.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, Enum, DateTime, ForeignKey
+from sqlalchemy import Column, String, Enum, DateTime, ForeignKey, Numeric
 from sqlalchemy.orm import relationship
 
 from core.database import Model
@@ -12,6 +12,13 @@ class ChargePoint(Model):
     description = Column(String(48), nullable=True)
     status = Column(Enum(ChargePointStatus), default=ChargePointStatus.OFFLINE, index=True)
     last_heartbeat = Column(DateTime, nullable=True)
+    manufacturer = Column(String, nullable=False)
+    latitude = Column(Numeric, nullable=False)
+    longitude = Column(Numeric, nullable=False)
+    serial_number = Column(String, nullable=False)
+    unavailability_reason = Column(String, nullable=False)
+    comment = Column(String, nullable=False)
+    model = Column(String, nullable=False)
 
     location_id = Column(String, ForeignKey("locations.id", ondelete="CASCADE"), nullable=False)
     location = relationship("Location", back_populates="charge_points")
